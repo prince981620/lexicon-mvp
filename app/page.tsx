ce56a0df64dacd2a1c135c35c055399fa8bb85d6
Guney Uzel <devtex16@gmail.com>
2024-11-19 15:15:06 +0300

    def

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       </div>
 
       <div className="flex-1 flex items-start justify-center px-4 pb-12">
-        <ChatComponent />
+        <ChatComponent configId="default" />
       </div>
     </main>
   );
